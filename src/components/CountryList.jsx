0f663bf6a9951d957d99122d4075dcd3bbfcbf98
Monica Coca <mcocapelaz@gmail.com>
2025-11-26 10:07:17 +0100

    key attribute  in CountryList.jsx fixed

--- src/components/CountryList.jsx
+++ src/components/CountryList.jsx
@@ -1,8 +1,8 @@
 
 function CountryList({countriesNames}) { return (
 <div className="country-info-app">
-  {countriesNames.map((country, index) => (
-  <div className="country-card" key={index}>
+  {countriesNames.map((country) => (
+  <div className="country-card" key={country.name}>
     <span className="flag">{country.flag}</span>
     <h2>{country.name}</h2>
   </div>
